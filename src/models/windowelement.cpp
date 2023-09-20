c9742b07774de0506ab21f4877c2371707aa91bd
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-09-20 13:48:49 +0800

    chore: windowelement and group

---
+++ src/models/windowelement.cpp
@@ -0,0 +1,79 @@
+#include "windowelement.h"
+#include <algorithm>
+
+WindowElement::WindowElement(const QString &id, QObject *parent)
+  : m_id(id)
+  , m_icon(QString())
+{
+}
+
+void
+WindowElement::deleteSelf()
+{
+    Q_EMIT selfDestory(this);
+    this->deleteLater();
+}
+
+void
+WindowElement::requestActiveChanged()
+{
+    // TODO:
+    Q_EMIT activeChanged();
+}
+
+void
+WindowElement::setIcon(const QString &icon)
+{
+    m_icon = icon;
+    Q_EMIT iconChanged();
+    Q_EMIT requestIconChanged(m_icon);
+}
+
+WindowElementGroup::WindowElementGroup(bool isPin, const QString &id, QObject *parent)
+  : m_isPin(isPin)
+  , m_id(id)
+  , m_elements({})
+{
+}
+
+void
+WindowElementGroup::appendWindowElement(WindowElement *element)
+{
+    connect(element, &WindowElement::selfDestory, this, &WindowElementGroup::removeWindowElement);
+    connect(element, &WindowElement::requestIconChanged, this, &WindowElementGroup::setIcon);
+    m_icon = element->icon();
+    m_elements.push_back(element);
+    Q_EMIT elementsChanged();
+}
+
+void
+WindowElementGroup::removeWindowElement(WindowElement *element)
+{
+    m_elements.removeOne(element);
+    if (m_elements.isEmpty() || !m_isPin) {
+        Q_EMIT deleteSelf(this);
+        return;
+    }
+    Q_EMIT elementsChanged();
+}
+
+void
+WindowElementGroup::setIcon(const QString &icon)
+{
+    m_icon = icon;
+    Q_EMIT iconChanged();
+}
+
+void
+WindowElementGroup::requestActiveChanged()
+{
+    QVector<WindowElement *>::iterator it = std::find_if(
+      m_elements.begin(), m_elements.end(), [](WindowElement *e) { return e->isActive(); });
+    if (it != m_elements.end()) {
+        (*it)->requestActiveChanged();
+        Q_EMIT activeChanged();
+        return;
+    }
+    m_elements[0]->requestActiveChanged();
+    Q_EMIT activeChanged();
+}
---
+++ src/models/windowelement.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <QObject>
+#include <QVector>
+
+class WindowElement : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit WindowElement(const QString &id, QObject *parent = nullptr);
+    ~WindowElement() = default;
+
+    Q_PROPERTY(bool isActive READ isActive NOTIFY activeChanged)
+    inline bool isActive() { return m_isActive; }
+
+    Q_PROPERTY(QString icon READ icon NOTIFY iconChanged)
+    inline QString icon() { return m_icon; }
+
+    inline QString id() { return m_id; }
+
+    Q_INVOKABLE void requestActiveChanged();
+
+public slots:
+    void deleteSelf();
+    void setIcon(const QString &icon);
+
+signals:
+    void activeChanged();
+    void requestIconChanged(const QString &);
+    void iconChanged();
+    void selfDestory(WindowElement *);
+
+private:
+    QString m_id;
+    bool m_isActive = true;
+    // INFO: base 64
+    QString m_icon;
+};
+
+class WindowElementGroup : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit WindowElementGroup(bool isPin, const QString &id, QObject *parent = nullptr);
+
+    Q_PROPERTY(QVector<WindowElement *> elements READ elements NOTIFY elementsChanged)
+    inline QVector<WindowElement *> elements() { return m_elements; }
+
+    Q_PROPERTY(QString icon READ icon NOTIFY iconChanged)
+    inline QString icon() { return m_icon; }
+
+    Q_PROPERTY(bool isPin READ isPin NOTIFY isPinChanged)
+    inline bool isPin() { return m_isPin; }
+
+    Q_PROPERTY(QString id READ id NOTIFY idChanged)
+    inline QString id() { return m_id; }
+
+    Q_INVOKABLE void requestActiveChanged();
+
+public slots:
+    void appendWindowElement(WindowElement *element);
+    void removeWindowElement(WindowElement *element);
+    void setIcon(const QString &icon);
+
+signals:
+    void elementsChanged();
+    void activeChanged();
+    void iconChanged();
+    void idChanged();
+    void isPinChanged();
+    void deleteSelf(WindowElementGroup *);
+
+private:
+    bool m_isPin;
+    QString m_id;
+    QVector<WindowElement *> m_elements;
+    // INFO: base 64, the actived icon
+    QString m_icon;
+};
